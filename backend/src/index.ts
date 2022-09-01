f768fa20a8a5083c4be93ffe71a5df7b684e11c7
Yuki Yuigishi <me@yuigishi.dev>
2022-09-01 20:57:14 +0900

    update

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,5 +5,6 @@ const app = new Hono();
 app.get("/", (c) => c.text("test"));
 
 app.get("/api", (c) => c.json({ message: "Hello World" }));
+app.get("/set", (c) => c.json({ message: "Hello World" }));
 
 app.fire();
