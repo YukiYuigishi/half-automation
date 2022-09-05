054f2239a7324418ce686b86c60559e0c6bc97e8
Yuki Yuigishi <me@yuigishi.dev>
2022-09-06 04:43:30 +0900

    chore: wip

---
+++ backend/src/api.test.ts
@@ -0,0 +1,12 @@
+import api from "./api";
+
+describe("Root", () => {
+   it("GET /", async () => {
+      /*
+      const res = await api.request("http://localhost/");
+      expect(res.status).toBe(200);
+      const body = await res.json();
+      expect(body).toEqual({ message: "Hello" });
+      */
+   });
+});
