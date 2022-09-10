6d5042657d46e433d5e91516ed5484a1582290b0
Yuki Yuigishi <me@yuigishi.dev>
2022-09-10 19:38:39 +0900

    chore(hard): feat call api test

--- meishiv1.0-hardware/src/main.cpp
+++ meishiv1.0-hardware/src/main.cpp
@@ -1,11 +1,12 @@
 #include <Arduino.h>
-#include <WiFi.h>
+#include <ArduinoJson.h>
 #include <HTTPClient.h>
-#include<ArduinoJson.h>
+#include <WiFi.h>
 
 const char *ssid = "TP-Link_3B04";
 const char *password = "63464320";
 
+String serverName = "https://half-automation.yuigishi.workers.dev/";
 String response = "";
 
 DynamicJsonDocument doc(2048);
@@ -42,8 +43,30 @@ void loop() {
           timeInfo.tm_mon + 1, timeInfo.tm_mday, timeInfo.tm_hour,
           timeInfo.tm_min, timeInfo.tm_sec); //人間が読める形式に変換
 
-  Serial.println(s);
-
+  String serverPath = serverName + "/1234567890";
   HTTPClient http;
+
+  http.begin(serverPath.c_str());
+
+  int httpResponseCode = http.GET();
+
+  if (httpResponseCode > 0) {
+    Serial.print("HTTP Response code: ");
+    Serial.println(httpResponseCode);
+    String payload = http.getString();
+
+    deserializeJson(doc, http.getStream());
+
+    Serial.println(doc["id"].as<String>());
+
+
+    Serial.println(payload);
+  } else {
+    Serial.print("Error Code: ");
+    Serial.println(httpResponseCode);
+  }
+
+  http.end();
+
   delay(5000);
 }
